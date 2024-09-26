0e37978516eadd67a47495efcc5e26c1cc386901
ritaaguerreiro <152714825+ritaaguerreiro@users.noreply.github.com>
2024-09-26 12:21:27 +0100

    Add files via upload

---
+++ Exercicio_2/pages/DadosForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DadosForm() {
+return (
+<section className="DadosForm">
+ <h1>Os dados inseridos no formulário surgirão aqui !</h1>
+
+</section>
+);
+}
+
+export default DadosForm;
