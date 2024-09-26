94e2b9a7d373ebf6586fb614ee2d5cb5f28caa6d
ritaaguerreiro <152714825+ritaaguerreiro@users.noreply.github.com>
2024-09-26 19:14:50 +0100

    DadosForm.js

--- Exercicio_2/pages/DadosForm.js
+++ Exercicio_2/pages/DadosForm.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import {useLocation} from "react-router-dom";
 
 function DadosForm() {
+const location = useLocation();
 return (
 <section className="DadosForm">
- <h1>Os dados inseridos no formulário surgirão aqui !</h1>
-
+ <h3>Os dados inseridos no formulário surgirão aqui !</h3>
+  <p> Melhor artista/banda escolhido {location.state.melhorart}</p>
+   <p> Sugestões {location.state.sugestoes}</p>
 </section>
 );
 }
