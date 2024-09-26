b10b8be011859fe4c5c347b69fd8e6dcc6c3feb2
ritaaguerreiro <152714825+ritaaguerreiro@users.noreply.github.com>
2024-09-26 22:29:49 +0100

    App.js

--- Exercicio_2/pages/App.js
+++ Exercicio_2/pages/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 import festaImage from '../fantasy.jpg';
@@ -7,7 +7,7 @@ import {useNavigate} from "react-router-dom";
 
 function Cabecalho() {
 return(<section id="head">
-<h1>Festa Fantasia</h1>
+<h1>Festa Fantasia &#129498;</h1>
 <img src={festaImage} alt="Imagem Tema da Festa"/>
 </section>);
 }
@@ -16,9 +16,9 @@ function ListaDasBandas(){
 return(
 <section id="bandas">
 <br/>
-<h2><b>ARTISTAS</b> que não vais querer perder:</h2>
+<h2><b>ARTISTAS</b> que não vais querer perder &#129311;</h2>
 <br/>
-<ul>
+<ul class="lista">
 <li>Shanin Blake</li>
 <li>Toy</li>
 <li>Aurora</li>
@@ -33,12 +33,14 @@ return(
 }
 
 function FormFesta(){
-  const navigate = useNavigate();
+     const navigate = useNavigate();
 
   const goToReceiver = () => {
-    navigate('/DadosForm', {
+    navigate('/dados-form', {
       state: {
-        melhorart: document.getElementById("melhorart").value,
+        melhorartista: document.getElementById("melhora").value,
+        melhoratividade: document.getElementById("atividades").value,
+        classificacao: document.getElementById("rating").value,
         sugestoes: document.getElementById("sugestao").value
       }
     });
@@ -46,96 +48,65 @@ function FormFesta(){
 
 return(
 
+
 <section id="feedback">
-<h2>Dá-nos o teu feedback sobre a festa...</h2>
-<p>O teu feedback é importante para nós :)</p>
+<br/> <br/>
+<h2>Dá-nos o teu feedback sobre a festa  &#128523;</h2>
+
+
 <form>
-
-<input type="checkbox" id="dia27" name="dia27" value="dia27" />
-    <label htmlFor="dia27">Dia 27</label>
-    <br /><br />
-
-    <input type="checkbox" id="dia28" name="dia28" value="dia28" />
-    <label htmlFor="dia28">Dia 28</label>
-    <br /><br />
-
-    <input type="checkbox" id="dia29" name="dia29" value="dia29" />
-    <label htmlFor="dia29">Dia 29</label>
-    <br /><br />
-
     <h3>Qual o artista/banda que mais gostaste de ouvir?</h3>
 
-<select id="melhorart" name="artist">
-  <option value="1">Shanin Blake (dia 27)</option>
-  <option value="2">Toy (dia 27)</option>
-  <option value="5">Aurora (dia 28)</option>
-  <option value="7">Rosinha (dia 29)</option>
-  <option value="3">Clairo (dia 27)</option>
-  <option value="6">D.A.M.A. (dia 28)</option>
-  <option value="8">Tunng (dia 29)</option>
-  <option value="8">Quim Barreiros (dia 29)</option>
-  <option value="8">Big Thief (dia 29)</option>
-  <option value="10">Não gostei de nenhum :(</option>
+<select id="melhora" name="artist">
+  <option value="Shanin Blake">Shanin Blake (dia 27)</option>
+  <option value="Toy">Toy (dia 27)</option>
+  <option value="Aurora">Aurora (dia 28)</option>
+  <option value="Rosinha">Rosinha (dia 29)</option>
+  <option value="Clairo">Clairo (dia 27)</option>
+  <option value="D.A.M.A.">D.A.M.A. (dia 28)</option>
+  <option value="Tunng">Tunng (dia 29)</option>
+  <option value="Quim Barreiros">Quim Barreiros (dia 29)</option>
+  <option value="Big Thief">Big Thief (dia 29)</option>
+  <option value="Nenhum :(">Não gostei de nenhum :(</option>
+</select>
+
+    <br /> <br />
+
+    <h3>Diz-nos a atividade que mais adoraste!</h3>
+
+   <select id="atividades" name="atividades" multiple size="5">
+    <option value="Roda Gigante">Roda Gigante</option>
+    <option value="Pinturas Faciais">Pinturas Faciais</option>
+    <option value="Oficina de Poções">Oficina de Poções</option>
+    <option value="Lançamento de Machados">Lançamento de Machados</option>
+    <option value="Touro Mecânico">Touro Mecânico</option>
+    <option value="Desfile de Fantasias">Desfile de Fantasias</option>
+    <option value="Nenhuma :(">Não gostei de nenhuma :(</option>
 </select>
 
     <br /><br />
 
-    <h3>Diz-nos a(s) atividade(s) que mais adoraste!</h3>
+<h3>Classifica a tua satisfação neste festival! (de 1 a 5 estrelas)</h3>
 
-    <input type="checkbox" id="SMR" name="option1" value="1" />
-    <label htmlFor="SMR">Roda Gigante </label>
-    <br /><br />
+    <label htmlFor="rating" style={{ fontFamily: "Georgia, sans-serif", fontSize: "20px" }}>Escolhe uma classificação: </label>
+    <select id="rating" name="rating">
+     <option value="1 estrela">★☆☆☆☆ (1 estrela)</option>
+     <option value="2 estrelas">★★☆☆☆ (2 estrelas)</option>
+     <option value="3 estrelas">★★★☆☆ (3 estrelas)</option>
+     <option value="4 estrelas">★★★★☆ (4 estrelas)</option>
+     <option value="5 estrelas">★★★★★ (5 estrelas)</option>
 
-    <input type="checkbox" id="RG" name="option2" value="2" />
-    <label htmlFor="RG">Pinturas Faciais</label>
-    <br /><br />
 
-    <input type="checkbox" id="SS" name="option3" value="3" />
-    <label htmlFor="SS">Oficina de Poções </label>
-    <br /><br />
+    </select>
+    <br /><br />     <br />
 
-    <input type="checkbox" id="LM" name="option4" value="4" />
-    <label htmlFor="LM">Lançamento de Machados</label>
-    <br /><br />
-
-    <input type="checkbox" id="TM" name="option5" value="5" />
-    <label htmlFor="TM">Touro Mecânico</label>
-    <br /><br />
-
-    <input type="checkbox" id="TM" name="option5" value="5" />
-    <label htmlFor="TM">Desfile de Fantasias </label>
-    <br /><br />
-
-    <input type="checkbox" id="NoneActivities" name="option6" value="6" />
-    <label htmlFor="NoneActivities">Não gostei de nenhuma :(</label>
-    <br /><br />
-
-    <h3>Classifica a tua satisfação neste festival! (de 1 a 5 estrelas)</h3>
-
-    <div className="rating" style={{ marginRight: '600px' }}>
-        <input type="radio" id="star5" name="rating" value="5" />
-        <label htmlFor="star5" title="5 estrelas">★</label>
-
-        <input type="radio" id="star4" name="rating" value="4" />
-        <label htmlFor="star4" title="4 estrelas">★</label>
-
-        <input type="radio" id="star3" name="rating" value="3" />
-        <label htmlFor="star3" title="3 estrelas">★</label>
-
-        <input type="radio" id="star2" name="rating" value="2" />
-        <label htmlFor="star2" title="2 estrelas">★</label>
-
-        <input type="radio" id="star1" name="rating" value="1" />
-        <label htmlFor="star1" title="1 estrela">★</label>
-        <br /><br />
-    </div>
-
-                 <label htmlFor="sugestao"> </label>
-                 <textarea type="textarea" id="sugestao" name="sugestao" rows="5" cols="50">Deixe aqui a sua sugestão de artistas e bandas, e como podemos melhorar para o próximo ano! </textarea>
+                 <label id="sugestao" htmlFor="sugestoes" > </label>
+                 <textarea type="textarea" id="sugestao" name="sugestao" rows="5" cols="50">Deixa aqui a tua sugestão de artistas e bandas, e de como podemos melhorar para o próximo ano! </textarea>
                  <br /><br />
 
-    <button onClick={goToReceiver} target="_blank">Clica aqui para verificares a tua resposta</button>
-
+ <br />
+    <button onClick={goToReceiver} class="botao">Clica aqui para verificares a tua resposta</button>
+ <br /><br />  <br /><br />
 </form>
 </section>
 );
@@ -151,6 +122,5 @@ function App() {
  );
 }
 export default App;
-
 
 
