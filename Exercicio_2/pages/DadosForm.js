dcff32f9e90a245065f1826076ee48e6e178eb3c
anasofia1003 <assaa6@iscte-iul.pt>
2024-09-27 23:06:32 +0100

    Update DadosForm.js

--- Exercicio_2/pages/DadosForm.js
+++ Exercicio_2/pages/DadosForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import '../DadosForm.css';
+import fairy from '../fairy_png1.png';
 
 function DadosForm() {
   const location = useLocation();
@@ -9,12 +10,15 @@ function DadosForm() {
     <section className="DadosForm">
       <h1>Os dados inseridos no formulário estão aqui!</h1>
       <br/>
-      <p><b>Melhor artista/banda escolhido: </b> {location.state?.melhorartista || "Sem resposta"}</p>
-      <p><b>Atividade escolhida: </b> {location.state?.melhoratividade || "Sem resposta"}</p>
-      <p><b>Classificação dada: </b> {location.state?.classificacao || "Sem resposta"}</p>
-      <p><b>Sugestões feitas: </b> {location.state?.sugestoes || "Sem resposta"}</p>
-      <br/>
+      <ul>
+      <li><b>Melhor artista/banda escolhido: </b> {location.state?.melhorartista || "Sem resposta"}</li>
+      <li><b>Atividade escolhida: </b> {location.state?.melhoratividade || "Sem resposta"}</li>
+      <li><b>Classificação dada: </b> {location.state?.classificacao || "Sem resposta"}</li>
+      <li><b>Sugestões feitas: </b> {location.state?.sugestoes || "Sem resposta"}</li>
+      </ul><br/>
       <h3>Obrigado! O teu feedback é importante para nós :)</h3>
+      <br/><br/><br/>
+      <img src={fairy} alt="Fada"/>
     </section>
   );
 }
