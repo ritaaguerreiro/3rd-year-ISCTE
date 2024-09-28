891cf804b0b50f28db278e5a18aef1b7238129fd
ritaaguerreiro <152714825+ritaaguerreiro@users.noreply.github.com>
2024-09-28 16:10:30 +0100

    DadosForm.js

--- Exercicio_2/pages/DadosForm.js
+++ Exercicio_2/pages/DadosForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import '../DadosForm.css';
-import fairy from '../fairy_png1.png';
+import fairy from "../fada1.png";
 
 function DadosForm() {
   const location = useLocation();
@@ -9,8 +9,9 @@ function DadosForm() {
   return (
     <section className="DadosForm">
       <h1>Os dados inseridos no formulário estão aqui!</h1>
+      <img src={fairy} alt="Fada" class="img" />
       <br/>
-      <ul>
+      <ul class="dados">
       <li><b>Melhor artista/banda escolhido: </b> {location.state?.melhorartista || "Sem resposta"}</li>
       <li><b>Atividade escolhida: </b> {location.state?.melhoratividade || "Sem resposta"}</li>
       <li><b>Classificação dada: </b> {location.state?.classificacao || "Sem resposta"}</li>
@@ -18,7 +19,6 @@ function DadosForm() {
       </ul><br/>
       <h3>Obrigado! O teu feedback é importante para nós :)</h3>
       <br/><br/><br/>
-      <img src={fairy} alt="Fada"/>
     </section>
   );
 }
